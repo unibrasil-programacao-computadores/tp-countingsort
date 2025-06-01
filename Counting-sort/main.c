6ce5b45a05190732009473eda42e0d0cc66158b9
Your Name <you@example.com>
2025-06-01 12:35:08 -0300

    implementação do counting com inteiros

--- Counting-sort/main.c
+++ Counting-sort/main.c
@@ -3,25 +3,29 @@
 
 int main()
 {
+
+    int arr[10] = {4, 7, 1, 9, 3, 2, 8, 5, 6, 1};
+    int tamanho = 10;
+
+    countingsort(arr, tamanho);
+
+    printf("array ordenado:\n");
+    for(int i = 0; i < tamanho; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+
 //    sort_base_array(quicksort, "Quicksort");
 //    sort_ordered_arrays(quicksort, "Quicksort");
 //    sort_reverse_ordered_arrays(quicksort, "Quicksort");
 //    sort_uniform_arrays(quicksort, "Quicksort");
 
-//    sort_base_array(selectionsort, "selectionsort");
-//    sort_ordered_arrays(selectionsort, "selectionsort");
-//    sort_reverse_ordered_arrays(selectionsort, "selectionsort");
-//    sort_uniform_arrays(selectionsort, "selectionsort");
 
-//    sort_base_array(insertionsort, "insertionsort");
-//    sort_ordered_arrays(insertionsort, "insertionsort");
-//    sort_reverse_ordered_arrays(insertionsort, "insertionsort");
-//    sort_uniform_arrays(insertionsort, "insertionsort");
-
-    sort_base_array(insertionsort_binario, "insertionsort_Binario");
-    sort_ordered_arrays(insertionsort_binario, "insertionsort_Binario");
-    sort_reverse_ordered_arrays(insertionsort_binario, "insertionsort_Binario");
-    sort_uniform_arrays(insertionsort_binario, "insertionsort_Binario");
+//    sort_base_array(insertionsort_binario, "insertionsort_Binario");
+//    sort_ordered_arrays(insertionsort_binario, "insertionsort_Binario");
+//    sort_reverse_ordered_arrays(insertionsort_binario, "insertionsort_Binario");
+//    sort_uniform_arrays(insertionsort_binario, "insertionsort_Binario");
 
 
     return 0;
