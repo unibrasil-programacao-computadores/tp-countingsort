4e0857d87259f3cf84baeb076e40ddf99ea40096
Ravel <ravelrcamargo@gmail.com>
2025-06-13 19:53:26 -0300

    Arrumei os graficos

--- sort/main.c
+++ sort/main.c
@@ -7,21 +7,27 @@ int main()
 {
 
 
-
     sort_base_array(countingsort, "countingsort");
     sort_ordered_arrays(countingsort, "countingsort");
     sort_reverse_ordered_arrays(countingsort, "countingsort");
     sort_uniform_arrays(countingsort, "countingsort");
 
-     printf("\nMemória total usada pelo Counting Sort: %.2f MB\n",
+
+
+    printf("\nMemória total usada pelo Counting Sort: %.2f MB\n",
            memoria_total_counting / (1024.0 * 1024.0));
 
 
-//
-//    sort_base_array(quicksort, "quicksort");
+//   sort_base_array(quicksort, "quicksort");
 //    sort_ordered_arrays(quicksort, "quicksort");
-//    sort_reverse_ordered_arrays(quicksort, "quicksort");
-//    sort_uniform_arrays(quicksort, "quicksort");
+//   sort_reverse_ordered_arrays(quicksort, "quicksort");
+//   sort_uniform_arrays(quicksort, "quicksort");
+
+
+//   sort_base_array(selectionsort, "selectionsort");
+//   sort_ordered_arrays(selectionsort, "selectionsort");
+//    sort_reverse_ordered_arrays(selectionsort, "selectionsort");
+//    sort_uniform_arrays(selectionsort, "selectionsort");
 
 
 
